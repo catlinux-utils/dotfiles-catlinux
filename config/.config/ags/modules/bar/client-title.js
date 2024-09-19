0b1d99d64d3707ed5838965b259d475e148c7740
none <none>
2024-09-19 12:48:24 +0200

    ags client title visible change

--- config/.config/ags/modules/bar/client-title.js
+++ config/.config/ags/modules/bar/client-title.js
@@ -4,6 +4,7 @@ function ClientTitle() {
   return Widget.Label({
     class_name: "client-title",
     label: hyprland.active.client.bind("title"),
+    visible: hyprland.active.client.bind("address").as((addr) => addr !== "0x"),
   });
 }
 
