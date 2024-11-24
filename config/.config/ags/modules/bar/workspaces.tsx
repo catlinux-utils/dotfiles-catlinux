8b80033b112117e46353beed6a9b2b3a94f46489
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-11-24 18:17:27 +0100

    fix workspaces and cleanup

--- config/.config/ags/modules/bar/workspaces.tsx
+++ config/.config/ags/modules/bar/workspaces.tsx
@@ -1,52 +1,66 @@
 import Hyprland from "gi://AstalHyprland";
-import { Variable, bind } from "astal";
 
 export default function Workspaces(): JSX.Element {
   const hyprland = Hyprland.get_default();
+
   return (
     <eventbox
-      onScroll={(_, e) => {
-        e.delta_y > 0
-          ? hyprland.dispatch("workspace", "+1")
-          : hyprland.dispatch("workspace", "-1");
+      onScroll={(_, event) => {
+        const direction = event.delta_y > 0 ? "+1" : "-1";
+        hyprland.dispatch("workspace", direction);
       }}
     >
       <box className="workspaces group">
-        {Array.from({ length: 10 }, (_, i) => i + 1).map((workspaceId) => {
-          return (
-            <button
-              className={`workspace${
-                hyprland.get_workspace(workspaceId)?.get_clients().length > 0
-                  ? " occupied"
-                  : ""
-              }${
-                hyprland.get_focused_workspace().get_id() === workspaceId
-                  ? " active"
-                  : ""
-              }`}
-              onClicked={() => hyprland.dispatch("workspace", `${workspaceId}`)}
-              setup={(self) => {
-                self.hook(hyprland, "event", (_, event) => {
-                  /*if (event === "openwindow" || event === "closewindow") {
-                    return self.toggleClassName(
-                      "occupied",
-                      hyprland.get_workspace(workspaceId)?.get_clients()
-                        .length > 0
-                    );
-                  }*/
-                  if (event === "workspacev2") {
-                    return self.toggleClassName(
-                      "active",
-                      hyprland.get_focused_workspace().get_id() === workspaceId
-                    );
-                  }
-                });
-              }}
-            >
-              {workspaceId}
-            </button>
-          );
-        })}
+        {Array.from({ length: 10 }, (_, index) => index + 1).map(
+          (workspaceNumber) => {
+            return (
+              <button
+                className={`workspace${
+                  hyprland.get_workspace(workspaceNumber)?.get_clients()
+                    .length > 0
+                    ? " occupied"
+                    : ""
+                }${
+                  hyprland.get_focused_workspace().get_id() === workspaceNumber
+                    ? " active"
+                    : ""
+                }`}
+                visible={
+                  workspaceNumber <= 9 ||
+                  hyprland.get_workspace(workspaceNumber) !== null
+                }
+                setup={(self) => {
+                  self.hook(hyprland, "event", (_, event) => {
+                    if (event === "openwindow" || event === "closewindow") {
+                      if (
+                        hyprland.get_focused_workspace().get_id() !==
+                        workspaceNumber
+                      ) {
+                        return;
+                      }
+
+                      self.toggleClassName(
+                        "occupied",
+                        (hyprland.get_workspace(workspaceNumber)?.get_clients()
+                          .length ?? 0) !== 0
+                      );
+                    }
+
+                    if (event === "workspacev2") {
+                      self.toggleClassName(
+                        "active",
+                        hyprland.get_focused_workspace().get_id() ===
+                          workspaceNumber
+                      );
+                    }
+                  });
+                }}
+              >
+                {workspaceNumber}
+              </button>
+            );
+          }
+        )}
       </box>
     </eventbox>
   );
