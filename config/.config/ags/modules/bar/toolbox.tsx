614158c317b9e87b54a956d519729dbf42db11d3
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2024-11-24 18:17:13 +0100

    repair toolbox

--- config/.config/ags/modules/bar/toolbox.tsx
+++ config/.config/ags/modules/bar/toolbox.tsx
@@ -1,35 +1,31 @@
-import { exec, execAsync } from "astal";
+import { execAsync } from "astal";
 
-export default function ToolBox() {
-  let idleEnabled = false;
-
-  execAsync(["bash", "-c", "pidof wayland-idle-inhibitor.py"]).then(
-    (output) => {
-      idleEnabled = Boolean(output.trim());
-    }
-  );
+export default function ToolBox(): JSX.Element {
+  let idleEnabled: boolean = false;
 
   return (
     <box className="tool-box group">
       <button
         className="item"
-        onClicked={() => execAsync(["bash", "-c", "hyprshot -m region -z"])}
+        onClicked={() => execAsync(["hyprshot", " -m ", "region", " -z"])}
       >
         <icon icon="applets-screenshooter-symbolic" />
       </button>
       <button
         className="item"
-        onClicked={(self) => {
+        onClicked={(self: any) => {
           idleEnabled = !idleEnabled;
+
           self.toggleClassName("item_enabled", idleEnabled);
           if (idleEnabled)
             execAsync([
               "bash",
               "-c",
               `pidof wayland-idle-inhibitor.py || ~/.config/hypr/scripts/wayland-idle-inhibitor.py`,
-            ]).catch(print);
-          else execAsync("pkill -f wayland-idle-inhibitor.py").catch(print);
+            ]).catch(printerr);
+          else execAsync("pkill -f wayland-idle-inhibitor.py").catch(printerr);
         }}
+        setup={(self: any) => self.toggleClassName("item_enabled", idleEnabled)}
       >
         <icon icon="clock" />
       </button>
