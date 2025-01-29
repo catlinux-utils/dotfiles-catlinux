7f19514b33b17ca2837513bf48b2348600d21bdc
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2025-01-29 14:36:50 +0100

    Update download-repo.sh

--- download-repo.sh
+++ download-repo.sh
@@ -15,24 +15,8 @@ if [ -f /etc/os-release ]; then
     
     case "${ID_LIKE:-$ID}" in
         arch|manjaro)
-            if ! command_exists yay && ! command_exists paru; then
-                echo "Installing yay as AUR helper..."
-                sudo pacman --noconfirm -S base-devel git
-                cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
-                cd yay-bin && makepkg --noconfirm -si
-            else
-                echo "AUR helper already installed"
-            fi
-            if command_exists yay; then
-                AUR_HELPER="yay"
-                elif command_exists paru; then
-                AUR_HELPER="paru"
-            else
-                echo "No AUR helper found. Please install yay or paru."
-                exit 1
-            fi
             echo "Installing "
-            $AUR_HELPER -S --noconfirm --needed $ARCH_DEPENDENCIES
+            sudo pacman -S --noconfirm --needed $ARCH_DEPENDENCIES
         ;;
     esac
 else
