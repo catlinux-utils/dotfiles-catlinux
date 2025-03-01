2d6e41c2c4aa29523e1ed0e6f0ae759306639493
ThePolishCat <88453875+ThePolishCat@users.noreply.github.com>
2025-03-01 16:44:31 +0100

    add yay instalation to pacman setup

--- setup-scripts/system-scripts/pacman-setup.sh
+++ setup-scripts/system-scripts/pacman-setup.sh
@@ -7,6 +7,11 @@ YELLOW='\e[33m'
 BLUE='\e[34m'
 
 ARCH_DEPENDENCIES="reflector sed base-devel git"
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 if [ -f /etc/os-release ]; then  
     . /etc/os-release
 
@@ -14,6 +19,15 @@ if [ -f /etc/os-release ]; then
         arch|manjaro)
             echo -e "${BLUE}Installing dependencies for Arch-based systems${RC}"
             sudo pacman -Sy --noconfirm --needed $ARCH_DEPENDENCIES
+
+             if ! command_exists yay && ! command_exists paru; then
+                echo -e "${YELLOW}Installing yay as AUR helper...${RC}"
+                sudo pacman --noconfirm -S base-devel git
+                cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
+                cd yay-bin && makepkg --noconfirm -si
+            else
+                echo -e "${GREEN}AUR helper already installed${RC}"
+            fi
         ;;
     esac
 else
@@ -35,7 +49,7 @@ echo -e "${GREEN}Fetching the fastest mirrors...${RC}"
 sudo reflector -l 25 --country Germany,Poland,Czechia --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose
 
 nc=$(grep -c ^processor /proc/cpuinfo)
-echo -ne "${BLUE}
+echo -e "${BLUE}
 -------------------------------------------------------------------------
                     You have ${nc} cores. And
             changing the makeflags for ${nc} cores. As well as
@@ -51,4 +65,3 @@ fi
 echo -e "${YELLOW}Disable options in makepkg.conf...${RC}"
 sudo sed -i '/^OPTIONS=/s/\b lto\b/ !lto/g' /etc/makepkg.conf
 sudo sed -i '/^OPTIONS=/s/\b debug\b/ !debug/g' /etc/makepkg.conf
-
