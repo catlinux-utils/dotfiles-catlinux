41126b8b21ec2c5383eee563943f80f1ab290b25
ThePolishCat <damianadam000@gmail.com>
2025-03-07 14:31:34 +0100

    update sudo setup

--- setup-scripts/system-scripts/sudo-setup.sh
+++ setup-scripts/system-scripts/sudo-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo sh -c "echo 'Defaults passwd_timeout=0' > /etc/sudoers.d/disable-timeout.conf"
-sudo sh -c "echo '%wheel      ALL=(ALL:ALL) ALL' > /etc/sudoers.d/enable-wheel.conf"
 
+sudo sed -i '/^# %wheel ALL=(ALL:ALL) ALL/ s/# //' /etc/sudoers
 
