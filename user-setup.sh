13613bce85053b09b5be1ce14a62fcec4fe53444
ThePolishCat <damianadam000@gmail.com>
2025-04-12 18:08:54 +0200

    new dep for dotfiles fix

--- user-setup.sh
+++ user-setup.sh
@@ -8,41 +8,39 @@ GREEN='\e[32m'
 DOTFILESDIR="$HOME/github/dotfiles-catlinux"
 
 HYPR="hyprland hyprlock hyprpaper hypridle hyprshot hyprpolkitagent xdg-desktop-portal-hyprland xdg-desktop-portal-gtk uwsm qt5-wayland qt6-wayland qt6ct-kde kvantum"
-UTILS="rofi-wayland wl-clipboard gnome-keyring polkit-gnome libgnome-keyring network-manager-applet xorg-xhost aylurs-gtk-shell-git adw-gtk-theme python-setproctitle"
+UTILS="rofi-wayland wl-clipboard gnome-keyring polkit-gnome libgnome-keyring network-manager-applet xorg-xhost aylurs-gtk-shell-git adw-gtk-theme python-setproctitle python-pywayland"
 APPS="kitty thorium-browser-bin code code-marketplace"
 SHELL="ttf-meslo-nerd zsh oh-my-zsh-git zsh-autosuggestions zsh-syntax-highlighting lsd bat cht.sh neovim"
 
-ARCH_DEPENDENCIES="${HYPR} ${UTILS} ${APPS} ${SHELL}" 
-
-
+ARCH_DEPENDENCIES="${HYPR} ${UTILS} ${APPS} ${SHELL}"
 
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-if [ -f /etc/os-release ]; then  
+if [ -f /etc/os-release ]; then
     . /etc/os-release
-    
+
     case "${ID_LIKE:-$ID}" in
-        arch|manjaro)
-            if ! command_exists yay && ! command_exists paru; then
-                echo "Installing yay as AUR helper..."
-                sudo pacman --noconfirm -S base-devel git
-                cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
-                cd yay-bin && makepkg --noconfirm -si
-            else
-                echo "AUR helper already installed"
-            fi
-            if command_exists yay; then
-                AUR_HELPER="yay"
-                elif command_exists paru; then
-                AUR_HELPER="paru"
-            else
-                echo "No AUR helper found. Please install yay or paru."
-                exit 1
-            fi
-            echo "Installing "
-            $AUR_HELPER -S --noconfirm --needed $ARCH_DEPENDENCIES
+    arch | manjaro)
+        if ! command_exists yay && ! command_exists paru; then
+            echo "Installing yay as AUR helper..."
+            sudo pacman --noconfirm -S base-devel git
+            cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
+            cd yay-bin && makepkg --noconfirm -si
+        else
+            echo "AUR helper already installed"
+        fi
+        if command_exists yay; then
+            AUR_HELPER="yay"
+        elif command_exists paru; then
+            AUR_HELPER="paru"
+        else
+            echo "No AUR helper found. Please install yay or paru."
+            exit 1
+        fi
+        echo "Installing "
+        $AUR_HELPER -S --noconfirm --needed $ARCH_DEPENDENCIES
         ;;
     esac
 else
@@ -70,7 +68,6 @@ else
     cd "$DOTFILESDIR"
     git pull
 fi
-
 
 # Backup existing neovim config and install new one
 mkdir -p "$DOTFILESDIR/backup/.config/"
