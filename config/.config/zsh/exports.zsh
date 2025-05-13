222fdc7f0af7b7ba63d28ec0ff6dd5a82b101612
ThePolishCat <damianadam000@gmail.com>
2025-05-13 21:33:27 +0200

    less fix

--- config/.config/zsh/exports.zsh
+++ config/.config/zsh/exports.zsh
@@ -6,6 +6,7 @@ export LESS_TERMCAP_se=$'\e[0m'       # Reset
 export LESS_TERMCAP_so=$'\e[1;44;33m' # Yellow on blue
 export LESS_TERMCAP_ue=$'\e[0m'       # Reset
 export LESS_TERMCAP_us=$'\e[1;32m'    # Green
+export LESS=-R
 
 export VISUAL=nano
 export EDITOR=nano
