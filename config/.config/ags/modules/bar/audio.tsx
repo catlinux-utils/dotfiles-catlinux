4f2441d58f4a0c85301ef1fe499e35a404989ccd
ThePolishCat <damianadam000@gmail.com>
2025-06-10 15:35:17 +0200

    add audio volume change on scroll

--- config/.config/ags/modules/bar/audio.tsx
+++ config/.config/ags/modules/bar/audio.tsx
@@ -3,14 +3,20 @@ import { bind } from "astal";
 
 export default function Audio() {
   const speaker = Wp.get_default()?.audio.defaultSpeaker!;
-  print(bind(speaker, "volume"));
 
   return (
-    <box className="audio group">
-      <icon className="item" icon={bind(speaker, "volumeIcon")} />
-      <label className="item">
-        {bind(speaker, "volume").as((v) => Math.floor(v * 100) + "%")}
-      </label>
-    </box>
+    <eventbox
+      onScroll={(_, event) => {
+        const direction = event.delta_y > 0 ? -0.01 : +0.01;
+        speaker.volume = Math.max(0, Math.min(1, speaker.volume + direction));
+      }}
+    >
+      <box className="audio group">
+        <icon className="item" icon={bind(speaker, "volumeIcon")} />
+        <label className="item">
+          {bind(speaker, "volume").as((v) => Math.floor(v * 100) + "%")}
+        </label>
+      </box>
+    </eventbox>
   );
 }
