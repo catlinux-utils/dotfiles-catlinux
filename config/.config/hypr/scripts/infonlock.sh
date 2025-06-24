5eaeb832111db259cb62b9b03a723c27c93d826f
ThePolishCat <damianadam000@gmail.com>
2025-06-24 20:45:06 +0200

    repair battery script

--- config/.config/hypr/scripts/infonlock.sh
+++ config/.config/hypr/scripts/infonlock.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-set -e
 
 # Get the current battery percentage
-battery_percentage=$(cat /sys/class/power_supply/BAT1/capacity)
+if [ -f "/sys/class/power_supply/BAT1/capacity" ]; then
+    battery_percentage=$(cat /sys/class/power_supply/BAT1/capacity)
+    battery_status=$(cat /sys/class/power_supply/BAT1/status)
 
-# Get the battery status (Charging or Discharging)
-battery_status=$(cat /sys/class/power_supply/BAT1/status)
+    # Define the battery icons for each 10% segment
+    battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
 
-# Define the battery icons for each 10% segment
-battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
+    # Define the charging icon
+    charging_icon="󰂄"
 
-# Define the charging icon
-charging_icon="󰂄"
+    if [ "$battery_percentage" -eq 100 ]; then
+        icon_index=9 # Set icon_index to 9 when battery_percentage is 100%
+    else
+        icon_index=$((battery_percentage / 10))
+    fi
 
-if [ "$battery_percentage" -eq 100 ]; then
-    icon_index=9  # Set icon_index to 9 when battery_percentage is 100%
+    # Get the corresponding icon
+    battery_icon=${battery_icons[icon_index]}
+
+    # Check if the battery is charging
+    if [ "$battery_status" = "Charging" ]; then
+        battery_icon="$charging_icon"
+    fi
+
+    # Output the battery percentage and icon
+    echo "$battery_percentage% $battery_icon"
 else
-    icon_index=$((battery_percentage / 10))
+    echo ""
 fi
-
-# Get the corresponding icon
-battery_icon=${battery_icons[icon_index]}
-
-# Check if the battery is charging
-if [ "$battery_status" = "Charging" ]; then
-	battery_icon="$charging_icon"
-fi
-
-# Output the battery percentage and icon
-echo "$battery_percentage% $battery_icon" 
