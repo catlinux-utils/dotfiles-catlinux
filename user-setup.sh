cad65c87739595a650c2e0000319f640740954c5
ThePolishCat <damianadam000@gmail.com>
2025-07-19 00:52:13 +0200

    setup dep update

--- user-setup.sh
+++ user-setup.sh
@@ -9,7 +9,7 @@ GREEN='\e[32m'
 DOTFILESDIR="$HOME/github/dotfiles-catlinux"
 
 HYPR="hyprland hyprlock hyprpaper hypridle hyprshot hyprpolkitagent xdg-desktop-portal-hyprland xdg-desktop-portal-gtk uwsm qt5-wayland qt6-wayland qt6ct-kde kvantum"
-UTILS="rofi-wayland wl-clipboard gnome-keyring polkit-gnome libgnome-keyring network-manager-applet xorg-xhost aylurs-gtk-shell-git libastal-meta cantarell-fonts adw-gtk-theme python-setproctitle python-pywayland playerctl dunst"
+UTILS="rofi-wayland wl-clipboard gnome-keyring polkit-gnome libgnome-keyring network-manager-applet xorg-xhost aylurs-gtk-shell-git libastal-meta dart-sass gtk-layer-shell blueprint-compiler cantarell-fonts adw-gtk-theme python-setproctitle python-pywayland playerctl dunst"
 APPS="kitty thorium-browser-bin code code-marketplace"
 SHELL="ttf-meslo-nerd zsh lsd bat cht.sh nano nano-syntax-highlighting"
 
