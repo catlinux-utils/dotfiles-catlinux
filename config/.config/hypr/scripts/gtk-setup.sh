7b6bc106578830d8ca02b09e7ab7054982f908c7
ThePolishCat <damianadam000@gmail.com>
2026-01-07 20:05:03 +0100

    script updates

--- config/.config/hypr/scripts/gtk-setup.sh
+++ config/.config/hypr/scripts/gtk-setup.sh
@@ -4,4 +4,4 @@ gsettings set org.gnome.desktop.interface font-name "Cantarell 10"
 gsettings set org.gnome.desktop.interface cursor-theme Adwaita
 gsettings set org.gnome.desktop.interface gtk-theme Materia-dark-compact
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.cinnamon.desktop.default-applications.terminal exec
+gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ cp -r config/. $HOME/
 
 gsettings set org.gnome.desktop.interface font-name "Cantarell 10"
 gsettings set org.gnome.desktop.interface cursor-theme Adwaita
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+gsettings set org.gnome.desktop.interface gtk-theme Materia-dark-compact
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
